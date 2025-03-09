14f8b8d41f80505b6046d9274c0261054acdaf10
Seibel <jpseibelcervo@yahoo.com.br>
2025-03-09 01:57:59 -0300

    Criação do Trabalho

---
+++ narutoGerenciamento/main.c
@@ -0,0 +1,41 @@
+/*
+Trabalho prático da revisão – v3
+
+Você foi designado para confeccionar um sistema de gerenciamento de
+ninjas e missões
+
+As entidades do sistema mais importantes são: ninjas e missões
+
+Cada ninja, possui um código (5 dígitos), nome e patente
+
+Cada missão possui um código (4 dígitos), nome, rank (D, C, B, A, S) e
+recompensa
+
+Seu sistema deve ser capaz de armazenar uma lista de ninjas e a lista de
+missões organizadas por período (por exemplo 1200 da Era Shinobi)
+
+Todas as estruturas devem ser alocadas dinamicamente
+
+Devem ser permitidas as opções de inserção e remoção em todas as
+listas, através de um menu de opções
+
+Além disso, devem ser permitidas consultas dos ninjas inscritos em uma
+determinada missão e das missões que determinados ninjas estão
+jogando, também em um determinado período.
+
+Finalmente, o sistema deve permitir o salvamento e a recuperação dos
+dados entre sessões
+
+Entregar arquivo (mínimo de 3 ninjas, missões e alocações)
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
