22e35b7c85303fff98b2c6e5c71b6844b23b4d64
Seibel <jpseibelcervo@yahoo.com.br>
2025-03-14 01:06:01 -0300

    Mensagem Inicial Engraçada

--- narutoGerenciamento/main.c
+++ narutoGerenciamento/main.c
@@ -31,12 +31,15 @@ Entregar arquivo (mínimo de 3 ninjas, missões e alocações)
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
 
 //Criando as estruturas no escopo global
 typedef enum {D,C,B,A,S} Rank;
 typedef enum {SemPatente, Genin, Chunin, Jonin} Patente;
+typedef enum {Fogo, Vento, Raio, Terra, Agua} Natureza; //Fogo, Vento, Raio, Terra, Água
 char *rankNomes[] = {"D", "C", "B", "A", "S"};
 char *patenteNomes[] = {"Sem Patente", "Genin", "Chunin", "Jonin"};
+char *naturezaNomes[] = {"Katon", "Fuuton", "Raiton", "Doton", "Suiton"};
 
 
 typedef struct ninjaStruct{
@@ -56,11 +59,60 @@ typedef struct missaoStruct{
 
 } Missao;
 
+void mensagemInicial();
+
 int main()
 {
-    ///Testando
+    setlocale(LC_ALL, "Portuguese");
+    mensagemInicial();
+
+    ///Verificar se há um arquivo?
+    ///Criar ou abrir arquivo (trataremos tudo em binário)
+
+    ///Ler arquivo
+    ///Puxar informações
+    ///Fechar arquivo
+
+    ///Rodar o sistema
+        ///Apresentação do menu()
+            //Opções do menu()
+            ///Printar todos os ninjas cadastrados
+            ///Printar todas as missões cadastradas
+            ///Adicionar ou remover um ninja
+            ///Adicionar ou remover uma missão
+            ///Editar um ninja......?
+            ///Editar uma missão....?
+            ///Printar um ninja - perfil completo
+
+    ///No caso de salvamento
+        ///Abrir o arquivo
+        ///Realizar alteração
+        ///Fechar arquivo
+
+    ///No caso de finalizar sessão no sistema
+        ///Liberar as memórias alocadas
+
+
     Rank rank = A;
     Patente patente = 3;
     printf("Rank do ninja: %s\nPatente do ninja: %s", rankNomes[rank], patenteNomes[patente]);
     return 0;
+}
+
+
+
+void mensagemInicial(){
+    for(int i=0;i<110;i++){
+        printf("#");
+    }
+
+    printf("\n\n\t\t\tSEJA BEM-VINDO AO SISTEMA GERAL DE REGISTRO DA ALIANÇA SHINOBI\n\n");
+    printf("\n\n\n\t   APÓS A 4ª GRANDE GUERRA NINJA, AS ALDEIAS NINJA ENCONTRARAM A PAZ E PERCEBERAM QUE TINHAM\t\n\tMUITO A GANHAR TRABALHANDO DE FORMA CONJUNTA, O QUE RESULTOU NA CRIAÇÃO DESTE SISTEMA UNIFICADO\t\n\n");
+    printf("\n\n\n\n\nVersão 1.0\tMente Criativa: Jiraya\tMente Operacional: Shikamaru\tResponsável pelos bugs: Obito\n");
+
+    for(int j=0;j<110;j++){
+        printf("#");
+    }
+    printf("\n\n");
+    return;
 }
